4c55bdd6577f566560959a4309bfa4c4efb4e7ab
daneshswara29 <44220709+daneshswara29@users.noreply.github.com>
2024-06-27 05:04:52 +0700

    Add files via upload

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# System Request : Debian 9+/Ubuntu 18.04+/20+
+# Develovers » Gemilangkinasih࿐
+# Email      » gemilangkinasih@gmail.com
+# telegram   » https://t.me/gemilangkinasih
+# whatsapp   » wa.me/+628984880039
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Gemilangkinasih࿐
+
 NS=$( cat /etc/xray/dns )
 PUB=$( cat /etc/slowdns/server.pub )
 domain=$(cat /etc/xray/domain)
@@ -9,27 +18,28 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/xhidrolix/scriptvpn/main/bot/bot.zip
+wget https://raw.githubusercontent.com/daneshswara29/install/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/xhidrolix/scriptvpn/main/bot/kyt.zip
+wget https://raw.githubusercontent.com/daneshswara29/install/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
 clear
 echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
+echo -e "\e[1;97;101m           » TAMBAH BOT PANEL «           \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
-echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
-echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
+echo -e "${grenbo}[»] Creat Bot and Token Bot : @BotFather${NC}"
+echo -e "${grenbo}[»] Info Id Telegram : @MissRose_bot${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-read -e -p "[*] Input your Bot Token : " bottoken
-read -e -p "[*] Input Your Id Telegram : " admin
+echo -e ""
+read -e -p "[»] Input your Bot Token   : " bottoken
+read -e -p "[»] Input Your Id Telegram : " admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
@@ -56,17 +66,19 @@ systemctl enable kyt
 systemctl restart kyt
 cd /root
 rm -rf kyt.sh
-echo "Done"
-echo "Your Data Bot"
-echo -e "==============================="
-echo "Token Bot         : $bottoken"
+echo "Input Data Berhasil Diproses!"
+echo "Your Data Bot Telegram"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "Token Bot      : $bottoken"
 echo "Admin          : $admin"
-echo "Domain        : $domain"
-echo "Pub            : $PUB"
-echo "Host           : $NS"
-echo -e "==============================="
-echo "Setting done"
+echo "Domain         : $domain"
+# echo "Pub            : $PUB"
+# echo "Host           : $NS"
+echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo "Setting Bot Success!"
 sleep 2
 clear
 
-echo " Installations complete, type /menu on your bot"
+echo "Installations complete, type /menu on your bot"
+read -p "Press Enter Back To menu"
+menu
